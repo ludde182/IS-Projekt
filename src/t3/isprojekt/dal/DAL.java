ed8f060a8300451f619d361bf869fab2118fc27e
Ludde <ludvig.ohlsson@gmail.com>
2015-09-21 17:42:29 +0200

    Student

--- src/t3/isprojekt/dal/DAL.java
+++ src/t3/isprojekt/dal/DAL.java
@@ -1,10 +1,22 @@
 package t3.isprojekt.dal;
+import java.sql.*;
 
 public class DAL {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void main (String[] args) {
+		try {
+			Connection con; 
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			String url = "jdbc:sqlserver://localhost:1433;database=isprojekt;user=root;password=root;";
+			con = DriverManager.getConnection(url);
+			System.out.println("Det lyckades!!");
+			}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
---
+++ src/t3/isprojekt/model/Student.java
@@ -0,0 +1,49 @@
+package t3.isprojekt.model;
+
+public class Student {
+	private int spnr;
+	private String name;
+	private String lastname;
+	private String address;
+	private int tfn;
+
+	public int getSpnr() {
+		return spnr;
+	}
+
+	public void setSpnr(int spnr) {
+		this.spnr = spnr;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getTfn() {
+		return tfn;
+	}
+
+	public void setTfn(int tfn) {
+		this.tfn = tfn;
+	}
+}
