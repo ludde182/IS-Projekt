e412b74daac4ce157a0ec897b22b5ed90d7d4b1f
pillepelle <ph_skold@hotmail.com>
2015-09-22 09:12:12 +0200

    aaa

---
+++ src/t3/isprojekt/model/Studied.java
@@ -0,0 +1,40 @@
+package t3.isprojekt.model;
+
+public class Studied {
+
+	private int sPnr;
+	private int cCode;
+	private int sBetyg;
+
+	private Studied(int sPnr, int cCode) {
+		setsPnr(sPnr);
+		setcCode(cCode);
+		setsBetyg(sBetyg);
+
+	}
+
+	public int getsPnr() {
+		return sPnr;
+	}
+
+	public void setsPnr(int sPnr) {
+		this.sPnr = sPnr;
+	}
+
+	public int getcCode() {
+		return cCode;
+	}
+
+	public void setcCode(int cCode) {
+		this.cCode = cCode;
+	}
+
+	public int getsBetyg() {
+		return sBetyg;
+	}
+
+	public void setsBetyg(int sBetyg) {
+		this.sBetyg = sBetyg;
+	}
+
+}
