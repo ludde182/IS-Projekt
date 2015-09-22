1e49c635fa87199b09506deb1433de6d0400798a
pillepelle <ph_skold@hotmail.com>
2015-09-22 09:07:55 +0200

    Laser

--- src/t3/isprojekt/model/laser.java
+++ src/t3/isprojekt/model/laser.java
@@ -1,6 +1,30 @@
 package t3.isprojekt.model;
 
 public class laser {
-	int hej = 0;
-	int tjo = 0;
-}
+
+	private int sPnr;
+	private int cCode;
+
+	public laser(int sPnr, int cCode) {
+		setsPnr(sPnr);
+		setcCode(cCode);
+
+	}
+
+	public int getcCode() {
+		return cCode;
+	}
+
+	public void setcCode(int cCode) {
+		this.cCode = cCode;
+	}
+
+	public int getsPnr() {
+		return sPnr;
+	}
+
+	public void setsPnr(int sPnr) {
+		this.sPnr = sPnr;
+	}
+
+};
