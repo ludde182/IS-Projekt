6a46307c5b48f52ca897fd95478a14d6cacf3686
siriaf <siiriaallflood@hotmail.com>
2015-09-22 11:34:23 +0200

    asd

--- src/t3/isprojekt/view/GUI.java
+++ src/t3/isprojekt/view/GUI.java
@@ -3,6 +3,7 @@ package t3.isprojekt.view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
 
 public class GUI {
 
@@ -36,8 +37,13 @@ public class GUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 700, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+
+		// TABBS
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(18, 17, 664, 639);
+		frame.getContentPane().add(tabbedPane);
 	}
 }
