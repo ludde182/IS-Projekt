1a110b6093303e0a464751dde5f32d0c1dea2972
siriaf <siiriaallflood@hotmail.com>
2015-09-22 09:17:06 +0200

    tjoho

---
+++ src/t3/isprojekt/model/Course.java
@@ -0,0 +1,32 @@
+package t3.isprojekt.model;
+
+public class Course {
+	private String cCode;
+	private int hp;
+	private String cDescription;
+
+	public String getcCode() {
+		return cCode;
+	}
+
+	public void setcCode(String cCode) {
+		this.cCode = cCode;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public String getcDescription() {
+		return cDescription;
+	}
+
+	public void setcDescription(String cDescription) {
+		this.cDescription = cDescription;
+	}
+
+}
