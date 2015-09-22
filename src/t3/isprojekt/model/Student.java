f7ce661006de3e694b460471b273cf710efbf4e7
pillepelle <ph_skold@hotmail.com>
2015-09-22 09:31:25 +0200

    dd

--- src/t3/isprojekt/model/Student.java
+++ src/t3/isprojekt/model/Student.java
@@ -1,49 +1,49 @@
 package t3.isprojekt.model;
 
 public class Student {
-	private int spnr;
-	private String name;
-	private String lastname;
-	private String address;
-	private int tfn;
+	private int sPnr;
+	private String sName;
+	private String sAdress;
+	private int sTfn;
 
-	public int getSpnr() {
-		return spnr;
+	private Student(int sPnr, String name, String sAdress, int sTfn) {
+		setsPnr(sPnr);
+		setsName(sName);
+		setsAdress(sAdress);
+		setsTfn(sTfn);
+
 	}
 
-	public void setSpnr(int spnr) {
-		this.spnr = spnr;
+	public int getsPnr() {
+		return sPnr;
 	}
 
-	public String getName() {
-		return name;
+	public void setsPnr(int sPnr) {
+		this.sPnr = sPnr;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public String getsName() {
+		return sName;
 	}
 
-	public String getLastname() {
-		return lastname;
+	public void setsName(String sName) {
+		this.sName = sName;
 	}
 
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
+	public String getsAdress() {
+		return sAdress;
 	}
 
-	public String getAddress() {
-		return address;
+	public void setsAdress(String sAdress) {
+		this.sAdress = sAdress;
 	}
 
-	public void setAddress(String address) {
-		this.address = address;
+	public int getsTfn() {
+		return sTfn;
 	}
 
-	public int getTfn() {
-		return tfn;
+	public void setsTfn(int sTfn) {
+		this.sTfn = sTfn;
 	}
 
-	public void setTfn(int tfn) {
-		this.tfn = tfn;
-	}
 }
