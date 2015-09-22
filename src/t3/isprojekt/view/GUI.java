689f455b75839c611bb5824099ab9695a20373e2
siriaf <siiriaallflood@hotmail.com>
2015-09-22 12:00:07 +0200

    Gui

--- src/t3/isprojekt/view/GUI.java
+++ src/t3/isprojekt/view/GUI.java
@@ -2,12 +2,20 @@ package t3.isprojekt.view;
 
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
 
 public class GUI {
 
 	private JFrame frame;
+	private JTextField textsName;
+	private JTextField textsPnr;
+	private JTextField textsAdress;
+	private JTextField textsTele;
 
 	/**
 	 * Launch the application.
@@ -46,5 +54,60 @@ public class GUI {
 		tabbedPane.setBounds(18, 17, 664, 639);
 		frame.getContentPane().add(tabbedPane);
 
+		JPanel student = new JPanel();
+		tabbedPane.addTab("Student", null, student, null);
+		student.setLayout(null);
+
+		JLabel lblsName = new JLabel("Namn: ");
+		lblsName.setBounds(56, 64, 61, 16);
+		student.add(lblsName);
+
+		JLabel lblsPnr = new JLabel("Pnr:");
+		lblsPnr.setBounds(56, 92, 61, 16);
+		student.add(lblsPnr);
+
+		JLabel lblsAdress = new JLabel("Adress:");
+		lblsAdress.setBounds(56, 118, 61, 16);
+		student.add(lblsAdress);
+
+		JLabel lblsTele = new JLabel("Tele:");
+		lblsTele.setBounds(56, 146, 61, 16);
+		student.add(lblsTele);
+
+		textsName = new JTextField();
+		textsName.setBounds(155, 58, 134, 28);
+		student.add(textsName);
+		textsName.setColumns(10);
+
+		textsPnr = new JTextField();
+		textsPnr.setBounds(155, 86, 134, 28);
+		student.add(textsPnr);
+		textsPnr.setColumns(10);
+
+		textsAdress = new JTextField();
+		textsAdress.setBounds(155, 112, 134, 28);
+		student.add(textsAdress);
+		textsAdress.setColumns(10);
+
+		textsTele = new JTextField();
+		textsTele.setBounds(155, 140, 134, 28);
+		student.add(textsTele);
+		textsTele.setColumns(10);
+
+		JButton btnAddCustomer = new JButton("L\u00E4gg till");
+		btnAddCustomer.setBounds(42, 206, 117, 29);
+		student.add(btnAddCustomer);
+
+		JButton btnSearch = new JButton("S\u00F6k");
+		btnSearch.setBounds(172, 206, 117, 29);
+		student.add(btnSearch);
+
+		JButton btnRemoveCustomer = new JButton("Ta bort");
+		btnRemoveCustomer.setBounds(301, 206, 117, 29);
+		student.add(btnRemoveCustomer);
+
+		JPanel course = new JPanel();
+		tabbedPane.addTab("Kurs", null, course, null);
+		course.setLayout(null);
 	}
 }
