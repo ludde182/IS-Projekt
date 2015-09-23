942630eba128a208be726088709294fa3d43b141
siriaf <siiriaallflood@hotmail.com>
2015-09-23 11:00:11 +0200

    Controller

---
+++ src/t3/isprojekt/controller/Controller.java
@@ -0,0 +1,16 @@
+package t3.isprojekt.controller;
+
+import t3.isprojekt.model.Course;
+import t3.isprojekt.model.Student;
+import t3.isprojekt.model.Studied;
+
+public class Controller {
+	private Course course;
+	private Student student;
+	private Studied studied;
+
+	public Controller() {
+
+	}
+
+}
