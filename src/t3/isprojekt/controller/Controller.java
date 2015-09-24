f7da3d742b6467f7de8e2822aa12cc88eec86c96
siriaf <siiriaallflood@hotmail.com>
2015-09-24 10:46:05 +0200

    KaozControll

--- src/t3/isprojekt/controller/Controller.java
+++ src/t3/isprojekt/controller/Controller.java
@@ -1,17 +1,23 @@
 package t3.isprojekt.controller;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import t3.isprojekt.dal.DAL;
 import t3.isprojekt.model.Course;
 import t3.isprojekt.model.Student;
 import t3.isprojekt.model.Studied;
+import t3.isprojekt.model.Studies;
 
 public class Controller {
 	private Course course;
 	private Student student;
 	private Studied studied;
+	private Studies studies;
 	private DAL dataAccessLayer;
+	private ArrayList<Student> studentList = new ArrayList<Student>();
+	private ArrayList<Studied> studiedList = new ArrayList<Studied>();
+	private ArrayList<Studies> studiesList = new ArrayList<Studies>();
 
 	public Controller() {
 		dataAccessLayer = new DAL();
@@ -54,5 +60,27 @@ public class Controller {
 	public void addCourseToStudies(String sPnr, String cCode)
 			throws SQLException {
 		dataAccessLayer.addStudentToStudies(sPnr, cCode);
+	}
+
+	public void removeStudentToStudies(String sPnr) throws SQLException {
+		dataAccessLayer.deleteStudentFromStudies(sPnr);
+	}
+
+	public ArrayList<Student> findResultOnCourse(String cCode)
+			throws SQLException {
+		studentList = dataAccessLayer.findResultOnCourse(cCode);
+		return studentList;
+	}
+
+	public ArrayList<Studied> findStudentResult(String pnr, String cCode)
+			throws SQLException {
+		studiedList = dataAccessLayer.findStudentResult(cCode, pnr);
+		return studiedList;
+	}
+
+	public String findPrecentageGrade(String cCode, String sGrade)
+			throws SQLException {
+		String percent = dataAccessLayer.findPercentageGradeA(cCode, sGrade);
+		return percent;
 	}
 }
